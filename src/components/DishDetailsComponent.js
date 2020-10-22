36c11c97c253d3bc8c92d94e0cae12009059787a
evangel <evangeldaping@gmail.com>
2020-10-21 20:24:01 -0800

    Single Page Applications Part 2

--- src/components/DishDetailsComponent.js
+++ src/components/DishDetailsComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     // componentDidMount() {
     //     console.log('Dishdetail Component componentDidMount invoked');
@@ -12,9 +12,9 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
     function RenderDish({dish}){
         return(
-            <div className="col-12 col-md-5 m-1">
+            <div className="col-12 col-md-12 m-1">
                 <Card>
-                    <CardImg src={dish.image} alt={dish.name}></CardImg>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
                     <CardBody>
                         <CardTitle>
                             {dish.name}
@@ -29,30 +29,26 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     }
 
     function RenderComments({comments}){
-        if (comments != null)
-        {
-            const commentListItem = comments.map((comment)=>{
-                return(
-                    <li key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit',}).format(new Date(Date.parse(comment.date)))}</p>
-                  </li>
-                );
-            });
-
+        if (comments != null)      
             return(
-                <div className="col-12 col-md-5 m-1">
+                <div className="col-12 col-md-12 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
-                        {commentListItem}
+                        {comments.map((comment) => {
+                            return (
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit',}).format(new Date(Date.parse(comment.date)))}</p>
+                              </li>
+                            );
+                        })}
                     </ul>
                 </div>
             );
-        }
-        else {
-            return(<div></div>);
-        }
-            
+        else 
+            return(
+            <div></div>
+        );            
     };
 
     const DishDetail = (props) => {
@@ -60,11 +56,25 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         // console.log('Dishdetail Component componentDidMount render invoked');
 
         if(props.dish != null) {
-            return(
-                <div class="container">
+            return (
+                <div className="container">
                     <div className="row">
-                        <RenderDish dish={props.dish} />
-                        <RenderComments comments={(props.dish.comments)} />
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} />
+                        </div>
                     </div>
                 </div>
             );
